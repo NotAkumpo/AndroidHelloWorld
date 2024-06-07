ca70fc8f2337737df79a0780503f6c66de752ea4
NotAkumpo <abdiel.evangelista@student.ateneo.edu>
2024-06-07 16:39:01 +0800

    Version 2: Added Application Life Cycle Demo

---
+++ ApplicationLifCycle2024/app/src/main/java/com/example/applicationlifcycle2024/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.applicationlifcycle2024;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    EditText input;
+    TextView myText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        click = findViewById(R.id.click);
+        input = findViewById(R.id.input);
+        myText = findViewById(R.id.myText);
+
+        myText.setText("Input Something");
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transferText();
+            }
+        });
+
+    }
+
+    public void transferText(){
+        String text = input.getText().toString();
+        myText.setText(text);
+    }
+
+    // https://developer.android.com/studio/debug/am-logcat
+
+    /*
+
+    These correspond the the log level dropdown on the Logcat
+
+    Log.d   Debug
+    Log.i   Info
+    Log.e   Error
+    Log.w   Warn
+    Log.v   Verbose
+
+    the first parameter is a TAG, this helps you filter using the Search box
+    (make sure Regex is deselected)
+     */
+
+    public void onStart()
+    {
+        super.onStart();
+        Log.d("Lifecycle", "onStart()");
+    }
+
+    public void onStop()
+    {
+        super.onStop();
+        Log.d("Lifecycle", "onStop()");
+
+    }
+
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.d("Lifecycle", "onRestart()");
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+        Log.d("Lifecycle", "onResume()");
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        Log.d("Lifecycle", "onPause()");
+    }
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("Lifecycle", "onDestroy()");
+    }
+}
